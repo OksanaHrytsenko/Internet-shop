6e4a3f55b06926c1b9fc00703cae2ab354ed53ef
Oksana <0682112130o@gmail.com>
2024-06-12 03:33:41 +0300

    Added search goods

--- src/goods/urls.py
+++ src/goods/urls.py
@@ -4,6 +4,7 @@ from goods.views import catalog, get_product
 app_name = 'goods'
 
 urlpatterns = [
+    path("search/", catalog, name="search"),
     path("<slug:category_slug>/", catalog, name="index"),
     path("product/<slug:product_slug>/", get_product, name="product"),
 ]
